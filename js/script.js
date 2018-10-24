693e3820157b500fe4f47bf58de36e441c2acf07
naksmedia <rioxpa@yandex.ru>
2018-10-24 15:21:02 +0300

    Верстка от 24 октября

--- js/script.js
+++ js/script.js
@@ -14,3 +14,53 @@ $(document).ready(function() {
       $(this).parent().removeClass('active');
    });
 });
+
+
+// Выбрать несколько элементов
+
+$('.sort').click(function () {
+    var mylist = $('.items');
+    var listitems = mylist.children('li').get();
+    listitems.sort(function (a, b) {
+      var compA = $(a).data('selected');
+      var compB = $(b).data('selected');
+      return (compA < compB) ? 1 : (compA > compB) ? 1 : 0;
+    });
+    $.each(listitems, function (idx, itm) { mylist.append(itm); });
+  })
+  
+  $('li', '.items').click(function (){
+    var checks = $('[type="checkbox"]', '.checks');
+    var item = $(this);
+    
+    if(item.data('selected') == '0') {
+      item.data('selected', '1');
+      item.addClass('selected');
+    } else {
+      item.data('selected', '0');
+      item.removeClass('selected');
+    }
+    
+    checks.filter('[data-guid="'+item.data('guid')+'"]').prop('checked', item.data('selected') == '1');
+  });
+  
+
+//   $('#jsMe').on('click', function() {
+//     $('#modal-close').modal('toggle');
+//     return false;
+// });
+
+// $('#modal-close').on('shown', function () {
+//     $('body').on('wheel.modal mousewheel.modal', function () {return false;});
+// }).on('hidden', function () {
+//     $('body').off('wheel.modal mousewheel.modal');
+// });
+
+
+function check() {
+    var submit = document.getElementsByName('submit')[0];
+    if (document.getElementById('politics').checked)
+    submit.disabled = '';
+    else
+    submit.disabled = 'disabled';
+    }
