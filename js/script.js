dd2012c8009d9c9035dda08cca8a1739cf0cdf37
naksmedia <rioxpa@yandex.ru>
2018-11-13 13:48:50 +0300

    Верстка от 13 ноября

--- js/script.js
+++ js/script.js
@@ -16,45 +16,7 @@ $(document).ready(function() {
 });
 
 
-// // Выбрать несколько элементов
 
-// $('.sort').click(function () {
-//     var mylist = $('.items');
-//     var listitems = mylist.children('li').get();
-//     listitems.sort(function (a, b) {
-//       var compA = $(a).data('selected');
-//       var compB = $(b).data('selected');
-//       return (compA < compB) ? 1 : (compA > compB) ? 1 : 0;
-//     });
-//     $.each(listitems, function (idx, itm) { mylist.append(itm); });
-//   })
-  
-//   $('li', '.items').click(function (){
-//     var checks = $('[type="checkbox"]', '.checks');
-//     var item = $(this);
-    
-//     if(item.data('selected') == '0') {
-//       item.data('selected', '1');
-//       item.addClass('selected');
-//     } else {
-//       item.data('selected', '0');
-//       item.removeClass('selected');
-//     }
-    
-//     checks.filter('[data-guid="'+item.data('guid')+'"]').prop('checked', item.data('selected') == '1');
-//   });
-  
-
-// //   $('#jsMe').on('click', function() {
-// //     $('#modal-close').modal('toggle');
-// //     return false;
-// // });
-
-// // $('#modal-close').on('shown', function () {
-// //     $('body').on('wheel.modal mousewheel.modal', function () {return false;});
-// // }).on('hidden', function () {
-// //     $('body').off('wheel.modal mousewheel.modal');
-// // });
 
 
 function check() {
@@ -112,3 +74,5 @@ $(document).ready(function () {
   });
 
 });
+
+
